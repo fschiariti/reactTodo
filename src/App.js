d15eaffc945e8a3155cfb1446395296857c8f146
Fernando <fernando.schiariti@gmail.com>
2020-08-11 16:47:39 -0300

    bind todos items

--- src/App.js
+++ src/App.js
@@ -25,11 +25,25 @@ class App extends Component {
     ]  
   }
 
+  markComplete = (id) => {
+    console.log(id);    
+
+
+    this.setState({ todos: this.state.todos.map(todo => {
+      if (todo.id ===id) {
+        todo.completed = !todo.completed;
+      }
+
+      return todo;
+    })
+  });
+  }
+
   render() {
     console.log(this.state.todos);
     return (
     <div className="App">
-          <Todos todos={this.state.todos}/>
+          <Todos todos={this.state.todos} markComplete= {this.markComplete}/>
         </div>
     );
   }
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -3,16 +3,18 @@ import TodoItem from './TodoItem';
 import PropTypes from 'prop-types'
 
 class Todos extends Component {
-  render() {
+
+    render() {
     console.log(this.props.todos)
     return this.props.todos.map((todo)=> (
-    <TodoItem key={todo.id} todo={todo}/>
+    <TodoItem key={todo.id} todo={todo} markComplete=
+    {this.props.markComplete}/>
     ));
   };
 
 }
 
-Todos.PropTypes = {
+Todos.propTypes = {
     todos: PropTypes.array.isRequired
 }
 
